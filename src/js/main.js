aa1ae6d83dcd218cc62ce0efd2771b300fb4b878
Максим Мольков <maksim994@yandex.ru>
2025-04-12 19:05:59 +0300

    up

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,17 @@
 addEventListener('DOMContentLoaded', (event) => {
 
+    /* Убрать */
+    if (document.querySelector('maps')) {
+        ymaps.ready(init);
+
+        function init() {
+            new ymaps.Map('maps', {
+                center: [55.76, 37.64],
+                zoom: 7,
+            });
+        }
+    }
+
     Fancybox.bind("[data-fancybox]", {
         autoFocus: false,
     });
